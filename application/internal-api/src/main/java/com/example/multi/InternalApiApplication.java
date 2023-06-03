f9e7718504c52c1d1d096954b8f95e55e95e1213
Y.Hun <ploki753@gmail.com>
2023-06-03 18:22:38 +0900

    멀티모듈 프로젝트 구성

---
+++ application/internal-api/src/main/java/com/example/multi/InternalApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+
+@SpringBootApplication
+public class InternalApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InternalApiApplication.class, args);
+    }
+}
